ff8b9b0ba7604341b95e8e0f20a9efb8e04c8cf9
MOH DEDE FAZRI <fazrithe@gmail.com>
2022-04-01 14:07:45 +0700

    update index
    update index

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         //Helpers
         App\Providers\UserServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
@@ -229,6 +230,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'UserHelp' => App\Helpers\User::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 
 ];
